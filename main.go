fbd1eaa0443c507e055753952d62ae314f8a5a15
Willow Carretero Chavez <willow@carreteroc.me>
2025-06-08 18:54:17 -0700

    Initial proof of concept

---
+++ main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+const URLRegex = `https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
+
+func makeUrlFixer(db *bolt.DB) func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		// Skip messages we sent ourselves.
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		// Skip messages without URLs.
+		re := regexp.MustCompile(URLRegex)
+		urlMatch := re.Find([]byte(m.Content))
+		if len(urlMatch) == 0 {
+			return
+		}
+
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(m.GuildID))
+			if b == nil {
+				return fmt.Errorf("bucket %v does not exist", m.GuildID)
+			}
+
+			fixedUrl := ""
+			b.ForEach(func(reStr, replaceWith []byte) error {
+				re := regexp.MustCompile(string(reStr))
+				if !re.Match(urlMatch) {
+					return nil
+				}
+				fixedUrl = string(re.ReplaceAll(urlMatch, replaceWith))
+
+				return nil
+			})
+
+			_, err := s.ChannelMessageSendReply(m.ChannelID, fixedUrl, m.Reference())
+			if err != nil {
+				fmt.Printf("error responding: %v\n", err)
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			log.Printf("error fixing URL in message: %v", err)
+		}
+	}
+}
+
+func makeRegisterFixer(db *bolt.DB) func(s *discordgo.Session, m *discordgo.InteractionCreate) {
+	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.ApplicationCommandData().Name != "register-fixer" {
+			return
+		}
+
+		matcher := ""
+		replaceWith := ""
+		options := i.ApplicationCommandData().Options
+		for _, opt := range options {
+			switch opt.Name {
+			case "matcher":
+				matcher = opt.StringValue()
+			case "replace-with":
+				replaceWith = opt.StringValue()
+			}
+		}
+
+		if matcher == "" || replaceWith == "" {
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Missing matcher or replace-with",
+				},
+			})
+			if err != nil {
+				log.Printf("could not respond to command: %v", err)
+			}
+
+			return
+		}
+
+		err := db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(i.GuildID))
+			if b == nil {
+				return fmt.Errorf("bucket %v does not exist", i.GuildID)
+			}
+
+			return b.Put([]byte(matcher), []byte(replaceWith))
+		})
+		if err != nil {
+			log.Printf("could not register fixer, (matcher: %v, replace-with: %v): %v", matcher, replaceWith, err)
+		}
+
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Registered fixer with matcher `%v` and replace-with `%v`", matcher, replaceWith),
+			},
+		})
+		if err != nil {
+			log.Printf("could not respond to command: %v", err)
+		}
+	}
+}
+
+func makeListFixers(db *bolt.DB) func(s *discordgo.Session, m *discordgo.InteractionCreate) {
+	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.ApplicationCommandData().Name != "list-fixers" {
+			return
+		}
+
+		fixers := map[string]string{}
+
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(i.GuildID))
+			if b == nil {
+				return fmt.Errorf("bucket %v does not exist", i.GuildID)
+			}
+			return b.ForEach(func(k, v []byte) error {
+				fixers[string(k)] = string(v)
+				return nil
+			})
+		})
+		if err != nil {
+			log.Printf("could not list fixers: %v", err)
+		}
+
+		responseBuilder := strings.Builder{}
+		responseBuilder.WriteString("Currently registered fixers:\n")
+		for k, v := range fixers {
+			responseBuilder.WriteString(fmt.Sprintf("- `%v`: `%v`\n", k, v))
+		}
+
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: responseBuilder.String(),
+			},
+		})
+		if err != nil {
+			log.Printf("could not respond to command: %v", err)
+		}
+	}
+}
+
+func main() {
+	authToken := flag.String("discord-auth-token", "", "Bot auth token")
+	mappingsDbPath := flag.String("mappings-db", "./mappings.db", "Path to mappings database")
+	flag.Parse()
+
+	db, err := bolt.Open(*mappingsDbPath, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err != nil {
+		log.Printf("error creating mappings bucket: %v", err)
+		return
+	}
+
+	discord, err := discordgo.New("Bot " + *authToken)
+	if err != nil {
+		log.Printf("error creating bot: %v", err)
+		return
+	}
+
+	discord.AddHandler(makeUrlFixer(db))
+	discord.AddHandler(makeRegisterFixer(db))
+	discord.AddHandler(makeListFixers(db))
+	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		for _, g := range r.Guilds {
+			db.Update(func(tx *bolt.Tx) error {
+				_, err := tx.CreateBucketIfNotExists([]byte(g.ID))
+				return err
+			})
+		}
+
+	})
+
+	// In this example, we only care about receiving message events.
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Printf("error opening connection: %v\n", err)
+		return
+	}
+
+	_, err = discord.ApplicationCommandCreate(discord.State.Application.ID, "", &discordgo.ApplicationCommand{
+		Name:        "register-fixer",
+		Description: "Register a URL fixer",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "matcher",
+				Description: "regex to match against URLs",
+				Type:        discordgo.ApplicationCommandOptionString,
+				Required:    true,
+			},
+			{
+				Name:        "replace-with",
+				Description: "replace URLs with this",
+				Type:        discordgo.ApplicationCommandOptionString,
+				Required:    true,
+			},
+		},
+	})
+	if err != nil {
+		log.Printf("error creating command: %v", err)
+		return
+	}
+
+	_, err = discord.ApplicationCommandCreate(discord.State.Application.ID, "", &discordgo.ApplicationCommand{
+		Name:        "list-fixers",
+		Description: "List URL fixers",
+	})
+	if err != nil {
+		log.Printf("error creating command: %v", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	discord.Close()
+}
